6623d5072023ed00e07a325e667ae3f46aed4c14
Victor Kalinkin <vakalinkin@hotmail.com>
2021-05-28 13:30:23 -0700

    added JS to set up local storage

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+var data = {
+  entries: [],
+  editing: null,
+  nextEntryId: 1
+};
+
+var previousEntries = localStorage.getItem('javascript-local-storage-days');
+
+if (previousEntries !== null) {
+  data = JSON.parse(previousEntries);
+}
+
+window.addEventListener('beforeunload', function (event) {
+  var dataJson = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage-days', dataJson);
+});
+
 var $addEntry = document.querySelector('.addEntry');
 var $modal = document.querySelector('.modal');
 
@@ -10,5 +27,6 @@ $addEntry.addEventListener('click', function (event) {
 var $form = document.querySelector('form');
 
 $form.addEventListener('submit', function (event) {
+
   $modal.className = 'modal hidden';
 });
